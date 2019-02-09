534a82e6d0d3a51f560fe134540a0bbfcc22d9ea
CJ Kenney <ckenney422@gmail.com>
2019-02-08 23:43:12 -0600

    add Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
-import React from "react"
+import React from 'react';
+import Header from '../components/Header';
+import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
-export default () => <div>Hello world!</div>
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default () => (
+  <Container>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Test title</title>
+      {/* TODO: Fix link */}
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
+    <Header />
+  </Container>
+);
