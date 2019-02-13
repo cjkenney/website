ebdf76d01c61c4c4ebbf522cee99e316518ab75a
CJ Kenney <ckenney422@gmail.com>
2019-02-13 00:05:35 -0600

    add gatsby-image and related config work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,21 @@
+var path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: `Test Title`
   },
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-react-helmet`]
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`)
+      }
+    },
+
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
+  ]
 };
